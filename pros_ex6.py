2d3f4b92a47123f4127f26389a175479a58a8f75
prosnob <pros.nob@student.passerellesnumeriques.org>
2021-02-26 10:21:40 +0700

    testin push

--- pros_ex6.py
+++ pros_ex6.py
@@ -1,29 +1,35 @@
 #Import-----------------------------------------------------------------------
 import tkinter as tk
 import random
+# import winsound
 #Windows----------------------------------------------------------------------
 root = tk.Tk() 
-root.geometry("600x600")
+root.geometry("700x800")
 frame = tk.Frame() 
 frame.master.title("pros.nob")
 canvas = tk.Canvas(frame)
 #Constants--------------------------------------------------------------------
-
+bg = tk.PhotoImage(file = "img/bg.png")
+bg_win = canvas.create_image(350,400,image=bg)
+pl = tk.PhotoImage(file = "img/player.png")
+player = canvas.create_image(350,650,image=pl)
 #Variable---------------------------------------------------------------------
 oval = canvas.create_oval(50,50,100,100, fill = "blue")
-rectangle = canvas.create_rectangle(235,530,365,550,fill="black")
-myImage = tk.PhotoImage(file="img/images.png")
-canvas.create_image(10,10,image=myImage)
-x = 10
-y =0
+# winsound.PlaySound("sound/playing.wav",winsound.SND_FILENAME) 
+# rectangle = canvas.create_rectangle(235,650,365,670,fill="black")
+x = 0
+y =10
 
 # x=0
 # y = 0
 # condition = True
+# IMG 
+
 # #Function---------------------------------------------------------------------
-# def myEventTrigger():
-# #    print("User have clicked at position:",event.x,event.y)
-#    global x,y,condition
+def myRectangle():
+#    print("User have clicked at position:",event.x,event.y)
+   global x,y,condition
+   
 #    if x<550 and y<550 and condition:
 #       x +=50
 #       y+=50
@@ -33,28 +39,25 @@ y =0
 #       y -=50
 #       if x==0 and y==0:
 #          condition = True
-#    randomColor = random.choice(colors)
-#    canvas.itemconfig(oval, fill = randomColor)
-#    canvas.moveto(oval,x,y)
-#    canvas.after(100,lambda:myEventTrigger())
+   canvas.move(oval,x,y)
+   canvas.after(100,lambda:myRectangle())
+   
 
-def moverectangle(event):
-    global x,y,positionrectangle
-    myImage = canvas.create_image(100,100,image=myImage)
-    positionrectangle=canvas.coords(rectangle)
-    if positionrectangle[0]<460:
-        canvas.move(myImage,x,y)
+def Right(event):
+    positionplayer=canvas.coords(player)
+    if positionplayer[0]<650:
+        canvas.move(player,30,0)
 def Left(event):
-    global x,y
-    myImage = canvas.create_image(100,100,image=myImage)
-    positionrectangle=canvas.coords(rectangle)
-    if positionrectangle[0]>0:
-        canvas.move(myImage,-x,y)
+    positionplayer=canvas.coords(player)
+    if positionplayer[0]>50:
+        canvas.move(player,-30,0)
 
 # canvas.tag_bind("PNCTarget","<Button-1>",myEventTrigger)
 # root.bind("<Button-1>",myEventTrigger)
-root.bind("<Right>",moverectangle)
+myRectangle()
+root.bind("<Right>",Right)
 root.bind("<Left>",Left)
+
 
 #Main--------------------------------------------------------------------------
 # myEventTrigger()
