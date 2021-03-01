a51997766160d980c5af017708900f11dc72f0eb
prosnob <pros.nob@student.passerellesnumeriques.org>
2021-03-02 01:40:59 +0700

    push 02/Mar/2021 1:40

--- pros.py
+++ pros.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 import random
 # import winsound
+# import winsound
 #Windows----------------------------------------------------------------------
 root = tk.Tk()
 root.geometry("700x800")
@@ -9,16 +10,18 @@ frame = tk.Frame()
 frame.master.title("pros.nob")
 canvas = tk.Canvas(frame)
 #Constants--------------------------------------------------------------------
+# winsound.PlaySound("sound/playing.wav",winsound.SND_FILENAME)
 position=[]
 positionplayer=[]
 bg = tk.PhotoImage(file = "img/bg.png")
-bg_win = canvas.create_image(350,400,image=bg)
+bg_win = canvas.create_image(350,400,image=bg)  
+# canvas.create_rectangle(0,0,800,100,fill="blue")
 pl = tk.PhotoImage(file = "img/player.png")
 player = canvas.create_image(350,650,image=pl)
 coin_image = tk.PhotoImage(file = "img/coin.png")
 coin = canvas.create_image(30,50,image=coin_image)
 lf =tk.PhotoImage(file="img/life.png")
-life = canvas.create_image(30,120 ,image=lf)
+life = canvas.create_image(30,110 ,image=lf)
 soundon =tk.PhotoImage(file="img/soundon.png")
 canvas.create_image(650,50 ,image=soundon,tags="sound")
 soundoff =tk.PhotoImage(file="img/soundoff.png")
@@ -26,16 +29,23 @@ life_image= tk.PhotoImage(file="img/life.png")
 diamon_image= tk.PhotoImage(file="img/diamon.png")
 boom_image= tk.PhotoImage(file="img/boom.png")
 
-diamon = canvas.create_image(30,180,image=diamon_image)
+diamon = canvas.create_image(30,170,image=diamon_image)
+
+pause = tk.PhotoImage(file="img/pause.png")
+canvas.create_image(650,90,image=pause,tags="pause")
+unpause = tk.PhotoImage(file="img/unpause.png")
 
 WINDOW_WIDTH = 700
 WINDOW_HEIGHT = 800
 COIN_SIZE = LIFE_SIZE=BOOM_SIZE=DIAMON_SIZE=30  # height = with
 soundX = 650
 soundY = 50
+pauseX = 650
+pauseY = 90
 #Variable--------------------------------------------- ------------------------
 soundCondition = True
 gameCondition = True
+pauseCondition = True
 allCoin =[]
 allLife =[]
 allDiamon =[]
@@ -45,9 +55,9 @@ coins = 0
 lives = 3
 diamons = 0
 booms = 0
-canvas.create_text(30,90,fill = "darkblue",font="Times 10 italic bold",text="Scores: "+str(coins),tags="inCreaseCoins")
-canvas.create_text(30,150,fill = "darkblue",font="Times 10 italic bold",text="Lives: "+str(lives),tags="de&in-CreaseLives")
-canvas.create_text(40,210,fill = "darkblue",font="Times 10 italic bold",text="Diamons: "+str(diamons),tags="inCreaseDiamons")
+canvas.create_text(30,85,fill = "white",font="Times 15 italic bold",text=str(coins),tags="inCreaseCoins")
+canvas.create_text(30,140,fill = "white",font="Times 15 italic bold",text=str(lives),tags="de&in-CreaseLives")
+canvas.create_text(30,200,fill = "white",font="Times 15 italic bold",text=str(diamons),tags="inCreaseDiamons")
 
 # #Function---------------------------------------------------------------------
 
@@ -56,33 +66,33 @@ canvas.create_text(40,210,fill = "darkblue",font="Times 10 italic bold",text="Di
 def addLife():
     global allLife
     lifeX = random.randrange(100,600)
-    allLife.append(canvas.create_image(lifeX,0,image=life_image,tags="coin"))
-    if gameCondition:
+    allLife.append(canvas.create_image(lifeX,-20,image=life_image,tags="coin"))
+    if gameCondition and pauseCondition: 
         canvas.after(30000,lambda:addLife())
     
 
 def addDiamon():
     global allDiamon
     diamonX = random.randrange(100,600)
-    allDiamon.append(canvas.create_image(diamonX,0,image=diamon_image,tags="coin"))
-    if gameCondition:
+    allDiamon.append(canvas.create_image(diamonX,-20,image=diamon_image,tags="coin"))
+    if gameCondition and pauseCondition:
         canvas.after(20000,lambda:addDiamon())
     
 
 def addBoom():
     global allBoom
     boomX = random.randrange(100,600)
-    allBoom.append(canvas.create_image(boomX,0,image=boom_image,tags="coin"))
-    if gameCondition:
+    allBoom.append(canvas.create_image(boomX,-20,image=boom_image,tags="coin"))
+    if gameCondition and pauseCondition:
         canvas.after(2000,lambda:addBoom())
     
 
 def addCoin():
     global allCoin
     coinX = random.randrange(100,600)
-    allCoin.append(canvas.create_image(coinX,0,image=coin_image,tags="coin"))
-    if gameCondition:
-        canvas.after(200,lambda:addCoin())
+    allCoin.append(canvas.create_image(coinX,-20,image=coin_image,tags="coin"))
+    if gameCondition and pauseCondition:
+        canvas.after(300,lambda:addCoin())
         moveCoin()
 
 # Removing Elements----------------------------------------------------------
@@ -115,12 +125,12 @@ def findCoinIndexAt(posX, posY):
         coordCoin = canvas.coords(allCoin[n])
         coinX = coordCoin[0]
         coinY = coordCoin[1]
-        if coinX + 15 > posX-45 and coinX-15 < posX+45 and  coinY +15 > posY-45 and coinY -15  < posY +45:
+        if coinX + 15 > posX-45 and coinX-15 < posX+45 and  coinY +15 > posY-45 and coinY -15  < posY +45:  
             return n
     return -1 # no coin found
 
 def findLifeIndexAt(posX, posY):
-    for n in range (len( allLife)):
+    for n in range (len( allLife)): 
         coordLife = canvas.coords(allLife[n])
         lifeX = coordLife[0]
         lifeY = coordLife[1]
@@ -157,9 +167,28 @@ def Sound(event):
         canvas.delete("sound")
         canvas.create_image(650,50 ,image=soundon,tags="sound")
         soundCondition = True
+def Pause(event):
+    global gameCondition,pauseCondition,pauseX,pauseY
+    if (event.x >= pauseX - 15 and event.x <= pauseX + 15) and (event.y >= pauseY - 15 and event.y <= pauseY + 15)and pauseCondition:
+        pauseCondition = False
+        canvas.delete("pause")
+        canvas.create_image(650,90 ,image=unpause,tags="pause")
+    elif (event.x >= pauseX - 15 and event.x <= pauseX + 15) and (event.y >= pauseY - 15 and event.y <= pauseY + 15)and not pauseCondition:
+        pauseCondition = True
+        if gameCondition:
+            moveCoin()
+            canvas.after(200,lambda:addCoin())
+            canvas.after(2000,lambda:addBoom())
+            canvas.after(20000,lambda:addDiamon())
+            canvas.after(30000,lambda:addLife())
+        canvas.delete("pause")
+        canvas.create_image(650,90 ,image=pause,tags="pause")
+        pauseCondition = True
+        
 def moveCoin():
     global coins,lives,diamons,booms,gameCondition
-    canvas.move("coin",0,20)
+    if gameCondition and pauseCondition:
+        canvas.move("coin",0,20)
 
     # 1 - Remove the first coin if outside of the window
     firstCoin = allCoin[0]
@@ -172,9 +201,9 @@ def moveCoin():
         removeCoin(coinIndexAtPlayer)
         # if lives != 0 and diamons != 3:
         if gameCondition:
-            coins +=1
+            coins +=1 
             canvas.delete("inCreaseCoins")
-            canvas.create_text(30,90,fill = "darkblue",font="Times 10 italic bold",text="Scores: "+str(coins),tags="inCreaseCoins")
+            canvas.create_text(30,85,fill = "white",font="Times 15 italic bold",text=str(coins),tags="inCreaseCoins")
             print("got coins",coins)
     # Remove Booms
     if len(allBoom)>0:
@@ -192,7 +221,7 @@ def moveCoin():
                     gameCondition = False
                 if lives >= 0 :   
                     canvas.delete("de&in-CreaseLives")
-                    canvas.create_text(30,150,fill = "darkblue",font="Times 10 italic bold",text="Lives: "+str(lives),tags="de&in-CreaseLives")
+                    canvas.create_text(30,140,fill = "white",font="Times 15 italic bold",text=str(lives),tags="de&in-CreaseLives")
                     print("got booms",booms)
                     # and diamons != 3
         # Remove Diamons
@@ -211,7 +240,7 @@ def moveCoin():
                     gameCondition  = False
                 if diamons <= 3 :
                     canvas.delete("inCreaseDiamons")
-                    canvas.create_text(40,210,fill = "darkblue",font="Times 10 italic bold",text="Diamons: "+str(diamons),tags="inCreaseDiamons")
+                    canvas.create_text(30,200,fill = "white",font="Times 15 italic bold",text=str(diamons),tags="inCreaseDiamons")
                     print("got diamons",diamons)
                     # and lives > 0
     # Remove Lifes
@@ -224,21 +253,21 @@ def moveCoin():
         if lifeIndexAtPlayer != -1 :
             removeLife(lifeIndexAtPlayer)
             if gameCondition:
-                # if lives <  3 and lives > 0 and diamons != 3 :
-                lives +=1
-                canvas.delete("de&in-CreaseLives")
-                canvas.create_text(30,150,fill = "darkblue",font="Times 10 italic bold",text="Lives: "+str(lives),tags="de&in-CreaseLives")
-                print("got lives",lives)
+                if lives <  3 and lives > 0 :
+                    lives +=1
+                    canvas.delete("de&in-CreaseLives")
+                    canvas.create_text(30,140,fill = "white",font="Times 15 italic bold",text=str(lives),tags="de&in-CreaseLives")
+                    print("got lives",lives)
 
 # Moving position player-----------------------------------------   
         
 def Right(event):
-    if gameCondition:
+    if gameCondition and pauseCondition:
         if getPlayerX() < 650:
             canvas.move(player,50,0)
 
 def Left(event):
-    if gameCondition:
+    if gameCondition and pauseCondition:
         if getPlayerX() > 50:
             canvas.move(player,-50,0)
 
@@ -254,8 +283,9 @@ addCoin()
 canvas.after(5000,lambda:addBoom())
 canvas.after(10000,lambda:addDiamon())
 canvas.after(20000,lambda:addLife())
-
+ 
 canvas.tag_bind("sound","<Button-1>",Sound)
+canvas.tag_bind("pause","<Button-1>",Pause)
 root.bind("<Right>",Right)
 root.bind("<Left>",Left)
 
